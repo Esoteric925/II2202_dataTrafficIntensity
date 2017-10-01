a6e0598dcae25e8659a9eb6b06e688a7bcd2e40d
Araxi Mekhitarian <araxii@live.se>
2017-10-01 17:00:34 +0200

    data packet intensity measurement done

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,25 +1,69 @@
 import com.google.gson.*;
-import org.json.*;
+import net.sf.json.JSON;
 
 import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
 
+        Map<String,Integer> pairs = new HashMap();
         JsonParser parser = new JsonParser();
 
-        Object obj = parser.parse(new FileReader("/Users/Amir/IdeaProjects/II2202_dataCenterTraffic/Measurements.json"));
+        Object measurement_obj = parser.parse(new FileReader("Measurements.json"));
+        Object path_obj = parser.parse(new FileReader("Paths.json"));
+        Object tracedir_obj = parser.parse(new FileReader("TraceDirections.json"));
 
-        JsonArray jsonArray = (JsonArray) obj;
+
+        JsonArray jsonArrayMeasurements = (JsonArray) measurement_obj;
+        JsonArray jsonArrayPaths = (JsonArray) path_obj;
+        JsonArray jsonArrayTraceDir = (JsonArray) tracedir_obj;
 
 
-        for (JsonElement js : jsonArray){
-            System.out.println("js "  + js.getAsJsonObject().get("idPath"));
+        for (JsonElement jsMeasurements : jsonArrayMeasurements){
+            int idPathM = jsMeasurements.getAsJsonObject().get("idPath").getAsInt();
+
+            for(JsonElement jsPaths : jsonArrayPaths){
+
+                int idPathP = jsPaths.getAsJsonObject().get("idPath").getAsInt();
+                if(idPathM == idPathP){
+                    int idTraceDirP = jsPaths.getAsJsonObject().get("idTraceDirection").getAsInt();
+
+                    for(JsonElement jsTraceDir : jsonArrayTraceDir){
+                        int idTraceDirT = jsTraceDir.getAsJsonObject().get("idTraceDirection").getAsInt();
+
+                        if(idTraceDirP == idTraceDirT){
+                            int source = jsTraceDir.getAsJsonObject().get("idSrc").getAsInt();
+                            int destination = jsTraceDir.getAsJsonObject().get("idDst").getAsInt();
+                            String pair = source + "-" + destination;
+
+
+                            if(pairs.containsKey(pair)){
+                                pairs.replace(pair, pairs.get(pair), pairs.get(pair) + 1);
+                            }else{
+                                pairs.put(pair, 1);
+                            }
+
+
+
+
+                        }
+                    }
+
+                }
+            }
+
+
         }
 
+        for (Map.Entry<String, Integer> entry : pairs.entrySet())
+        {
+            System.out.println(entry.getKey() + "/" + entry.getValue());
+        }
     }
 }
