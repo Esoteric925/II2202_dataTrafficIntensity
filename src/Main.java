1ce246713e8c2bbdb950a81531bb4440ade4d35f
Esoteric925 <haval925@gmail.com>
2017-10-01 14:55:27 +0200

    new Main

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
+import org.json.*;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("hej");
+    JSONObject jsonObject = new JSONObject();
+
+
     }
 }
